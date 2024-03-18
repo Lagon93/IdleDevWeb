c625b83310c33cf2157fa90a907f691f4841ad0c
Enriclop11 <informenric@gmail.com>
2024-03-18 15:09:37 +0100

    Generation of LC display
    A simple function to show the generation of LC by second

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Stats({ lc }) {
+function Stats({ lc, lcGeneration }) {
   return (
     <div className="stats">
       <img src="/lc_icon.png" alt="LC Icon" />
       <p>LC: {lc}</p>
+        <p>LC/S: {lcGeneration}</p>
     </div>
   );
 }

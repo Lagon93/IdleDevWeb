2283acdde31e7f3da191407c2cfb09959321bd81
Enriclop11 <informenric@gmail.com>
2024-04-03 13:38:22 +0200

    Added Models and Json Upgrades

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import lc from '../model/LC';
 
-function Stats({ lc, lcGeneration }) {
+function Stats() {
+    const [lcValue, setLcValue] = useState(lc.lc);
+    const [lcGeneration, setLcGeneration] = useState(lc.lcGeneration);
+
+    useEffect(() => {
+        lc.subscribe(() => {
+            setLcValue(lc.lc);
+            setLcGeneration(lc.lcGeneration);
+        });
+    }, []);
+
   return (
     <div className="stats">
       <img src="/lc_icon.png" alt="LC Icon" />
-      <p>LC: {lc}</p>
+      <p>LC: {lcValue}</p>
         <p>LC/S: {lcGeneration}</p>
     </div>
   );
